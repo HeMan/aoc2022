05c063b90b6a444ceb20415d75b1ce9f0890e161
Jimmy Hedman <jimmy.hedman@gmail.com>
2022-11-08 22:55:52 +0100

    Initial checkin

---
+++ day_01/main/day_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+
+using std::cout;
+using std::endl;
+
+extern "C" void app_main(void)
+{
+	while (true) {
+
+		cout << "Hello world day 01" << endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+}
