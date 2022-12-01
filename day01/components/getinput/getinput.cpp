5a11b9fcc32237a84669eb0e356a8c3be61c4f01
Jimmy Hedman <jimmy.hedman@gmail.com>
2022-12-01 06:19:06 +0100

    Update inputfile helpers

--- day01/components/getinput/getinput.cpp
+++ day01/components/getinput/getinput.cpp
@@ -3,14 +3,15 @@
 #include <sstream>
 
 
-std::vector<std::string> getlines(void) {
-	extern const unsigned char input_start[] asm("_binary_input_txt_start");
-	extern const unsigned char input_end[] asm("_binary_input_txt_end");
-	std::stringstream st(std::string(input_start, input_end-1));
+extern const unsigned char input_start[] asm("_binary_input_txt_start");
+extern const unsigned char input_end[] asm("_binary_input_txt_end");
+std::istringstream inputstream(std::string(input_start, input_end-1));
 
+std::vector<std::string> getlines(void) {
 	std::vector<std::string> retval;
 	std::string line;
-	while (std::getline(st, line)) {
+
+	while (std::getline(inputstream, line)) {
 		retval.push_back(line);
 	}
 
--- day01/components/getinput/getinput.h
+++ day01/components/getinput/getinput.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <sstream>
 
-
+extern std::istringstream inputstream;
 std::vector<std::string> getlines(void);
