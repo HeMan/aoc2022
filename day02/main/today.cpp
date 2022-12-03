4508e9e9e21a7bf18f9601e74765c3a40ee30b65
Jimmy Hedman <jimmy.hedman@gmail.com>
2022-12-03 06:50:27 +0100

    Day 2 completed

--- day02/main/today.cpp
+++ day02/main/today.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
+#include <map>
+#include <numeric>
 #include <thread>
-#include "getinput.h"
 
+#include "getinput.h"
 
 using std::cout;
 using std::endl;
 
-extern "C" void app_main(void)
-{
+std::map<std::string, int> scoremap_part1{
+	{"A X", 4}, {"A Y", 8}, {"A Z", 3},
+	{"B X", 1}, {"B Y", 5}, {"B Z", 9},
+	{"C X", 7}, {"C Y", 2}, {"C Z", 6},
+};
+
+
+std::map<std::string, int> scoremap_part2{
+	{"A X", 3}, {"A Y", 4}, {"A Z", 8},
+	{"B X", 1}, {"B Y", 5}, {"B Z", 9},
+	{"C X", 2}, {"C Y", 6}, {"C Z", 7},
+};
+
+extern "C" void app_main(void) {
 	std::vector<std::string> inputlines;
+	uint32_t part1, part2 = 0;
+
 	inputlines = getlines();
 	while (true) {
+		part1 = std::transform_reduce(inputlines.begin(), inputlines.end(), 0,
+																	std::plus{}, [](auto v) { return scoremap_part1[v]; });
 
-		std::cout << inputlines.size() << std::endl;
-		cout << "Hello world day02" << endl;
+		part2 = std::transform_reduce(inputlines.begin(), inputlines.end(), 0,
+																	std::plus{}, [](auto v) { return scoremap_part2[v]; });
+		cout << "Score in part 1 " << part1 << endl;
+		cout << "Score in part 2 " << part2 << endl;
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 }
